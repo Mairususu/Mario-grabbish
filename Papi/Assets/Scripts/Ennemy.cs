ff27c67271d3767f49d139667d87c1d241fc3edf
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2025-03-22 10:21:06 +0100

    marche

--- Papi/Assets/Scripts/Ennemy.cs
+++ Papi/Assets/Scripts/Ennemy.cs
@@ -5,32 +5,22 @@ using UnityEngine;
 public class Ennemy : MonoBehaviour
 {
 
-    public enum type { Fire = 1, Nature = 2, Thunder = 3, Water = 4 };  
-    // j'inscris les deux là, je fais un constructeur qui permet de leur attribuer à leur création, puis je leur attache le gameobject qu'il faut en fonction de leur type
     
-    public enum klass { Melee = 1, Archer = 2 };
-
-
     [SerializeField] public int pv;
     [SerializeField] public int speed;
     [SerializeField] public int damage;
-    [SerializeField] public type element;
-    [SerializeField] public klass comportement;
+    [SerializeField] public GameObject element;
+    [SerializeField] public GameObject klass;
 
 
-    public Ennemy(klass k, element e)
-    {
-        
-    }
-    
-    
-    protected void death(){
+
+/*    protected void death(){
         if (element != type.Fire)
         {
             Destroy(GameObject);
         }
     }
-    
+*/    
     
     // Start is called before the first frame update
     void Start()
