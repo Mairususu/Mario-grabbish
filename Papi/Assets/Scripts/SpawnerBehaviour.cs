19f8d783ff3a1484ea6b4d6bd57644120abd11f2
bobinnard <skyland1360@gmail.com>
2025-03-22 19:18:27 +0100

    spawner fait
    C'était pas si dur en vrai

--- Papi/Assets/Scripts/SpawnerBehaviour.cs
+++ Papi/Assets/Scripts/SpawnerBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SpawnerBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject[] ennemis;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    [SerializeField] private Ennemy[] ennemis;
     [SerializeField] private float cooldown;
     [SerializeField] private float cooldownTimer;
 
@@ -15,7 +17,9 @@ public class SpawnerBehaviour : MonoBehaviour
         canSpawn = false;
         yield return new WaitForSeconds(cooldown);
         if (cooldown > 0.5) cooldown -= cooldownTimer;
-        Instantiate(ennemis[Random.Range(0, ennemis.Length)], transform.position, Quaternion.identity);
+        Ennemy newEnnemy = Instantiate(ennemis[Random.Range(0, ennemis.Length)], transform.position, Quaternion.identity);
+        newEnnemy.Player_1 = player1;
+        newEnnemy.Player_2 = player2;
         canSpawn = true;
     }
     
