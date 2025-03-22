14161fa8e5f4f8863a6af46c97af0bb780d756c8
bobinnard <skyland1360@gmail.com>
2025-03-22 12:10:01 +0100

    système d'arme
    il spawn les projectiles pour le moment

---
+++ Papi/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    enum Element{
+        Fire,
+        Water,
+        Lightning,
+        Nature
+    }
+	[SerializeField] private Sprite LightningPlayer;
+	[SerializeField] private Sprite FirePlayer;
+	[SerializeField] private Sprite WaterPlayer;
+	[SerializeField] private Sprite NaturePlayer;
+    [SerializeField] private Element myElement;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private bool canShoot = true;
+    public float cadance;
+    
+    [SerializeField] private GameObject projectile; 
+
+    private IEnumerator Shoot(){
+        canShoot = false;
+        Instantiate(projectile, transform.position, transform.rotation);
+        yield return new WaitForSecond(cadance); 
+        canShoot = true;
+    }
+
+    private void UpdateSprite(){
+        if(myElement == Fire){
+            spriteRenderer.Sprite = FirePlayer;
+        }
+        if(myElement == Lightning){
+            spriteRenderer.Sprite = LightningPlayer;
+        }
+        if(myElement == Water){
+            spriteRenderer.Sprite = WaterPlayer;
+        }
+        if(myElement == Nature){
+            spriteRenderer.Sprite = NaturePlayer;
+        }
+    }
+
+    void Start()
+    {
+        UpdateSprite();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && canShoot){
+            StarCoroutine(Shoot());
+        }
+    }
+}
