59303ecf907b34070b8069992be5c2e7479cfe80
Mairususu <miles.espiand2004@gmail.com>
2025-03-22 17:33:57 +0100

    hwckvjldb sgjkb;, sgskjsgbgkjf

--- Papi/Assets/Scripts/DeathMenu.cs
+++ Papi/Assets/Scripts/DeathMenu.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DeathMenu : MonoBehaviour
 {
+    public AudioSource SFX;
     // Start is called before the first frame update
     
     void Start()
@@ -11,9 +12,13 @@ public class DeathMenu : MonoBehaviour
         gameObject.SetActive(false);
     }
     
-    // Update is called once per frame
-    void Update()
+    public void ToMain(){
+        SFX.Play();
+        SceneManager.LoadScene("Bait");
+    }
+    public void QuitB()
     {
-        
+        SFX.Play();
+        Application.Quit();
     }
 }
--- Papi/Assets/Scripts/HPSystem.cs
+++ Papi/Assets/Scripts/HPSystem.cs
@@ -1,15 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPSystem : MonoBehaviour
 {
-    [SerializeField] private LifePointsSystem lp;
+    public Image fill;
+    private static int pvs;
+    private static int pvmax=50;
+    public GameObject DeathM;
+
+    private void Start()
+    {
+        pvs = pvmax;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        lp.pvs -= 5;
-        if(lp.pvs <= 0){lp.pvs = 0;}
-        if(lp.pvs==0){lp.GameOver();}
+        pvs -= 5;
+        if(pvs <= 0){pvs = 0;}
+
+        Damage();
+        if(pvs==0){GameOver();}
+    }
+
+    private void Damage()
+    {
+        fill.rectTransform.anchorMax.Set(pvs/pvmax,1);
+        
+    }
+    public void GameOver()
+    {
+        Debug.Log("MAMA MIA");
+        Time.timeScale = 0;
+        DeathM.SetActive(false);
     }
 }
