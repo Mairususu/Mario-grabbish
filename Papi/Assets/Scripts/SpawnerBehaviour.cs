281c941f880a69ed346f8a0885a3f31647689969
bobinnard <skyland1360@gmail.com>
2025-03-22 19:54:13 +0100

    spawners
    spawners²

--- Papi/Assets/Scripts/SpawnerBehaviour.cs
+++ Papi/Assets/Scripts/SpawnerBehaviour.cs
@@ -6,7 +6,7 @@ public class SpawnerBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject player1;
     [SerializeField] private GameObject player2;
-    [SerializeField] private Ennemy[] ennemis;
+    [SerializeField] private GameObject[] ennemis;
     [SerializeField] private float cooldown;
     [SerializeField] private float cooldownTimer;
 
@@ -17,9 +17,9 @@ public class SpawnerBehaviour : MonoBehaviour
         canSpawn = false;
         yield return new WaitForSeconds(cooldown);
         if (cooldown > 0.5) cooldown -= cooldownTimer;
-        Ennemy newEnnemy = Instantiate(ennemis[Random.Range(0, ennemis.Length)], transform.position, Quaternion.identity);
-        newEnnemy.Player_1 = player1;
-        newEnnemy.Player_2 = player2;
+        GameObject newEnnemy = Instantiate(ennemis[Random.Range(0, ennemis.Length)], transform.position, Quaternion.identity);
+        newEnnemy.GetComponent<Ennemy>().Player_1 = player1;
+        newEnnemy.GetComponent<Ennemy>().Player_2 = player2;
         canSpawn = true;
     }
     
