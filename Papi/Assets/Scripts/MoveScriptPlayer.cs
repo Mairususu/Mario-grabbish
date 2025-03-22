f1f328d5f8f642056bdaf9a08a3d4fded9f5bfb6
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2025-03-22 19:56:30 +0100

    fix partiel des ennemis

--- Papi/Assets/Scripts/MoveScriptPlayer.cs
+++ Papi/Assets/Scripts/MoveScriptPlayer.cs
@@ -1,27 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerNumber
+{
+	Player1,
+	Player2
+}
+
 public class MoveScriptPlayer : MonoBehaviour
 {
+	public static MoveScriptPlayer instanceP1;
+	public static MoveScriptPlayer instanceP2;
+	
 	[SerializeField] Rigidbody2D rb;
-    public int numJoueur;
+    public PlayerNumber numJoueur;
 	public float moveSpeed;
 	public float horizontalMovement;
 	public float verticalMovement;
 	[SerializeField] private SpriteRenderer spriteRenderer;
-	
+
+	private void Awake()
+	{
+		if (numJoueur == PlayerNumber.Player1)
+		{
+			if (instanceP1 != null) Destroy(gameObject);
+			else instanceP1 = this;
+		}
+		else
+		{
+			if (instanceP2 != null) Destroy(gameObject);
+			else instanceP2 = this;
+		}
+	}
+
 	void Update(){
-		if (numJoueur == 1){
+		if (numJoueur == PlayerNumber.Player1){
 			horizontalMovement = Input.GetAxis("Horizontal_Player1") * moveSpeed * Time.deltaTime;
 			verticalMovement = Input.GetAxis("Vertical_Player1") * moveSpeed * Time.deltaTime;}
-		if (numJoueur == 2){
+		if (numJoueur == PlayerNumber.Player2){
 			horizontalMovement = Input.GetAxis("Horizontal_Player2") * moveSpeed * Time.deltaTime;
 			verticalMovement = Input.GetAxis("Vertical_Player2") * moveSpeed * Time.deltaTime;}
 
 		if (horizontalMovement > 0) spriteRenderer.flipX = true;
 		else spriteRenderer.flipX = false;
-		
+	
 		transform.position += new Vector3(horizontalMovement, verticalMovement,0);
 	}
 }
